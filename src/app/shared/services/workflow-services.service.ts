b5c0002bdca39ab3df06b01bccac216b9f65ffdd
Tushar Malakar <35859780+TusharMalakar@users.noreply.github.com>
2020-01-04 21:59:07 -0500

    workflowModel

--- src/app/shared/services/workflow-services.service.ts
+++ src/app/shared/services/workflow-services.service.ts
@@ -1,4 +1,4 @@
-import { WorkflowModel } from './../models/workflowModule';
+import { WorkflowModel } from './../models/workflowModel';
 import { environment } from '../../../environments/environment';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
